eff3fe940de58dee23c91fa1738415040ec15961
Erik Ahlund <erik@ahlund.me>
2025-11-19 08:34:33 +0100

    Display a wall

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, hello_world)
+        .add_plugins(DefaultPlugins
+            .set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, create_map)
         .run();
 }
 
-fn hello_world() {
-    println!("Hello, World!");
+fn create_map(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands.spawn(Sprite {
+        image: asset_server.load("sprites/wall.png"),
+        custom_size: Some(Vec2::new(100., 100.)),
+        ..default()
+    });
 }
+
+#[derive(Component)]
+pub struct Wall;
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Component)]
+pub struct Box;
+
+#[derive(Component)]
+pub struct BoxSpot;
